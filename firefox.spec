uriesk/firefox-jxl-rpmspec

commit 36a09da5bc1a14f6af3fe163c84f9852e23c44c8

build fix

firefox.spec
@@ -170,7 +170,7 @@ Source1:        firefox-langpacks-%{version}%{?pre_version}-20230925.tar.xz
 Source2:        cbindgen-vendor.tar.xz
 Source10:       firefox-mozconfig
 Source12:       firefox-redhat-default-prefs.js
-Source20:       firefox.desktop
+Source20:       org.mozilla.firefox.desktop
 Source21:       firefox.sh.in
 Source23:       firefox.1
 Source24:       mozilla-api-key
@@ -1009,7 +1009,7 @@ fi
 %dir %{_sysconfdir}/%{name}/*
 %dir %{_datadir}/mozilla/extensions/*
 %dir %{_libdir}/mozilla/extensions/*
-%{_datadir}/applications/%{name}.desktop
+%{_datadir}/applications/*.desktop
 %{_datadir}/metainfo/*.appdata.xml
 %{_datadir}/gnome-shell/search-providers/*.ini
 %{_datadir}/dbus-1/services/*

org.mozilla.firefox.search-provider.ini
@@ -1,5 +1,5 @@
 [Shell Search Provider]
-DesktopId=firefox.desktop
+DesktopId=org.mozilla.firefox.desktop
 BusName=org.mozilla.firefox.SearchProvider
 ObjectPath=/org/mozilla/firefox/SearchProvider
 Version=2

commit b85953896b4c0099084baa03c64ecd4aeb52d0c9

Don't include -x11 and -wayland desktop files in the main package (rhbz#2242523)
Regression caused by 36a09da5bc1a14f6af3fe163c84f9852e23c44c8

firefox.spec
@@ -169,7 +169,7 @@ ExcludeArch: i686
 Summary:        Mozilla Firefox Web browser
 Name:           firefox
 Version:        118.0.1
-Release:        6%{?pre_tag}%{?dist}
+Release:        7%{?pre_tag}%{?dist}
 URL:            https://www.mozilla.org/firefox/
 License:        MPLv1.1 or GPLv2+ or LGPLv2+
 Source0:        https://archive.mozilla.org/pub/firefox/releases/%{version}%{?pre_version}/source/firefox-%{version}%{?pre_version}.source.tar.xz
@@ -1053,7 +1053,7 @@ fi
 %dir %{_sysconfdir}/%{name}/*
 %dir %{_datadir}/mozilla/extensions/*
 %dir %{_libdir}/mozilla/extensions/*
-%{_datadir}/applications/*.desktop
+%{_datadir}/applications/firefox.desktop
 %{_datadir}/metainfo/*.appdata.xml
 %{_datadir}/gnome-shell/search-providers/*.ini
 %{_datadir}/dbus-1/services/*
@@ -1119,6 +1119,9 @@ fi
 #---------------------------------------------------------------------
 
 %changelog
+* Fri Oct 06 2023 Alessandro Astone <ales.astone@gmail.com> - 118.0.1-7
+- Don't include -x11 and -wayland desktop files in the main package (rhbz#2242523)
+
 * Fri Oct 6 2023 Martin Stransky <stransky@redhat.com>- 118.0.1-6
 - Removed DBusActivatable flag from desktop file as it crashes KDE (rhbz#2242454).
 